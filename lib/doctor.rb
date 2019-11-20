04cff9c18d82e11bc82010d3ee8d0c43b4e415b4
Salem Asrat <salemasrat@gmail.com>
2019-11-20 19:12:52 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,33 +1,27 @@
-
 class Doctor
 
-  attr_reader :name, :appointment, :patient
-
-  @@all_doc = []
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    @@all_doc << self
-  end
-
-  def self.all
-    @@all_doc
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
+    @@all << self
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
-    end
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
   def patients
-    appointments.map do |appointment|
-      appointment.patients
-    end
+    appointments.map {|appointment| appointment.patient}
   end
 
-end
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end 
